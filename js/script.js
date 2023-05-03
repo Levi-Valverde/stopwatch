efa2f942f7871d24101e74f7af22dd219c8162ff
levi <levivalverdelima@gmail.com>
2023-05-03 20:03:07 -0300

    colocandos os arquivos CSS e iniciando o JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const minutes = document.querySelector("#minutes");
+const minutes = document.querySelector("#seconds");
+const minutes = document.querySelector("#milliseconds");
+const minutes = document.querySelector("#startBtn");
+const minutes = document.querySelector("#pauseBtn");
+const minutes = document.querySelector("#resumeBtn");
+const minutes = document.querySelector("#resetBtn");
